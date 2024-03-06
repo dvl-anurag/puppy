c81ee4ca5a0b01f12add520cea2e388f8a946cb7
dvl-anurag <anurag.singh@digivatelabs.com>
2024-03-06 12:29:57 +0530

    added two more funcs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dvl-anurag/puppy
 
 go 1.21.4
+
+require github.com/dvl-anurag/dog v0.0.0-20240306065449-e2cff9232326
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/dvl-anurag/dog"
+
 func Bark() string {
 	return "woof"
 }
 
 func Barks() string {
 	return "woof woof woof woof"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
